ec4c43d9afa26cd61952ee0a8fc29ac0c9a1ce01
unknown <tijnvandenbergh2@hotmail.com>
2018-09-21 10:58:18 +0200

    First commit

---
+++ FFTInformatica/Models/ResponseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FFTInformatica.Models
+{
+    public class ResponseContext : DbContext
+    {
+        public ResponseContext(DbContextOptions<ResponseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Response> Responses { get; set; }
+    }
+}
